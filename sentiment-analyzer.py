932edc0905d082a9203de7139a3223fe1f73e42e
awomann <hello.awomann@gmail.com>
2026-03-13 18:32:48 -0400

    Add API setup and environment variables

--- sentiment-analyzer.py
+++ sentiment-analyzer.py
@@ -1,8 +1,12 @@
-# Reaching into the transformers library and pulling out one specific tool called pipeline; installed the transformer library via the terminal using pip install transformers. The library is from the Hugging Face libraries.
-from transformers import pipeline 
+import os
+import requests
+from dotenv import load_dotenv
 
-# Calling that pipeline tool and telling it "I want to do sentiment analysis." Behind the scenes it goes out and downloads a pre-trained model that already knows how to analyze sentiment. Then it packages everything up and stores it in your classifier variable, ready to use.
-classifier = pipeline("sentiment-analysis")
+load_dotenv()
+api_token = os.getenv("HF_TOKEN")
 
-result = classifier("I hate traveling.")
-print(result)
+API_URL = "https://api-inference.huggingface.co/models/distilbert/distilbert-base-uncased-finetuned-sst-2-english"
+
+headers = {
+    "Authorization" : "Bearer " + api_token
+}
